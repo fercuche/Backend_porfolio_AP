c900701f058efda99f9747d905d764479e8af1fe
fercuche <fer2.maciel@gmail.com>
2022-11-08 23:38:39 -0300

    second deploy

--- src/main/java/ar/edu/ap/portfolio/PortfolioApplication.java
+++ src/main/java/ar/edu/ap/portfolio/PortfolioApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -48,7 +49,7 @@ public class PortfolioApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
 
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
